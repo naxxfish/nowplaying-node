5af7c7d141d15c1f68aab4157fc57e8942234217
Chris Roberts <chris@naxxfish.eu>
2014-08-26 21:00:37 +0100

    Added first API method, /version

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ var app = express();
 var compression = require('compression')
 var debug = require('debug')('nowplaying-node');
 var MongoClient = require('mongodb').MongoClient;
-
+var VERSION = "0.0.1"
 var config = require('./config')
+var moment = require('moment')
 
 if (!config)
 {
@@ -17,6 +18,14 @@ MongoClient.connect(config.mongo.connectionString, function (err, db)
 {
 	app.use( compression({ threshold: 512 }) )
 	app.use(express.static(__dirname + '/wwwroot'));
+	app.get('/version', function (req, resp)
+	{
+		resp.end(JSON.stringify({
+			application: 'node-nowplaying',
+			version: VERSION,
+			author: "Chris Roberts"
+		}))
+	});
 	var server = app.listen(3000, function () {
 		console.log("Listening on port %d", server.address().port)
 	})
