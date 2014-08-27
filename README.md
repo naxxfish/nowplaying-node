7221567e8bb138735c485da938d33999209fad4d
Chris Roberts <chris@naxxfish.eu>
2014-08-27 15:09:30 +0100

    First commit of icecast feeding

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ First, copy `config.js.EXAMPLE` to `config.js` and edit it to suit your needs.
 `node index.js` 
 
 should do it
+
+### icecast_reader.js
+
+run this to feed your nowplaying engine with stuff off an Icecast mount.  Note that this only works for MP3 streams at the moment!
+     node icecast_reader.js
---
+++ icecast_reader.js
@@ -0,0 +1,24 @@
+var icecast = require('icecast')
+var devnull = require('dev-null')
+var request = require('request')
+var url = "http://catnap.in:8000/sustainer.mp3"
+var config = require('./config')
+
+icecast.get(url, function (res) {
+	console.error(res.headers)
+	res.on('metadata', function (metadata)
+	{
+		console.log("Got some metadata!")
+		var parsed = icecast.parse(metadata)
+		console.error(parsed)
+		var parts = parsed.StreamTitle.split(/ - /)
+		request.post("http://localhost:3000/feed").form({
+			'setTrack': 'on',
+			'track.artist': parts[0],
+			'track.title': parts[1],
+			'feedSecret': config.feedSecret
+		})
+	})
+		
+	res.pipe(devnull())
+})
